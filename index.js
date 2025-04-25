3f8a2d548b1fde166e9b37adbec60ea5324c47cc
Dal S Garbuja Pun <7892627+dal-gp@users.noreply.github.com>
2025-04-25 21:11:17 +1000

    reading and writing files

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const fs = require("fs");
+const { text } = require("stream/consumers");
 
 const hello = "Hello world";
 console.log(hello);
+const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+console.log(textIn);
+
+const textOut = `This is what we know about the avocado: ${textIn}\nCreate on ${Date.now()}`;
+fs.writeFileSync("./txt/output.txt", textOut);
+
